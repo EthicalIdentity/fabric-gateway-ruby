84a2f8a6fcaecab25347a80aef1d93a1713844a3
Jonathan Chan <jonathan.chan@ethicalidentity.com>
2021-12-21 23:27:13 -0600

    disable code coverage outside of CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 require "bundler/setup"
-
-require 'simplecov'
-SimpleCov.start
-
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+if ENV['CI'] == "true"
+  require 'simplecov'
+  SimpleCov.start
+  
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 require "fabric"
 
